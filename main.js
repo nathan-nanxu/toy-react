bd6585e43db72b891e6c4b31817414895f4342a2
徐楠 <nathanxu@jokusunokisiMac.gateway>
2020-09-26 18:03:27 +1000

    finish

--- main.js
+++ main.js
@@ -126,6 +126,10 @@ class Square extends Component {
   // ========================================
   
  render(<Game />, document.getElementById("root"));
+
+//  let game = <Game />;
+
+//  console.log(game.vdom);
   
   function calculateWinner(squares) {
     const lines = [
